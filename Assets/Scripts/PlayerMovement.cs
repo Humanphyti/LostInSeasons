9c7ea08201d5e54583898f7adcdbc3f41855a4ba
Humanphyti <humanphyti@gmail.com>
2019-09-06 11:37:54 -0700

    implmented jump, left and right movement
    basic movement is working. there is a lot of tweaking to be done to get the feel right but we have a scene to tailor that feel.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,16 +5,27 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public PlayerController controller;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public float runSpeed = 40f;
+    //move left and right
+    private float horizontalMove = 0f;
+
+    private bool jump = false;
 
     // Update is called once per frame
     void Update()
     {
-        Input.GetAxisRaw("Horizontal");
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
     }
 }
